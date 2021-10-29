ef743f6ac87bdafb679833b2032582548be75e02
Md-Alamin-hub <86272640+Md-Alamin-hub@users.noreply.github.com>
2021-10-29 13:03:31 +0600

    added firebase

--- src/components/Pages/Services/Services.js
+++ src/components/Pages/Services/Services.js
@@ -7,7 +7,7 @@ import Service from "../Service/Service";
 const Services = () => {
   const [services, setServices] = useState([]);
   useEffect(() => {
-    fetch("http://localhost:5000/services")
+    fetch("https://stormy-basin-87659.herokuapp.com/services")
       .then((res) => res.json())
       .then((data) => setServices(data));
   }, []);
