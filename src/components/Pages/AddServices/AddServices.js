27244cb74b15bf48f77fe51b898e60aa1ab26f03
Md-Alamin-hub <86272640+Md-Alamin-hub@users.noreply.github.com>
2021-10-31 00:38:45 +0600

    addservice added

--- src/components/Pages/AddServices/AddServices.js
+++ src/components/Pages/AddServices/AddServices.js
@@ -1,9 +1,30 @@
+import axios from "axios";
 import React from "react";
+import { useForm } from "react-hook-form";
+import "./AddServices.css";
 
 const AddServices = () => {
+  const { register, handleSubmit, reset } = useForm();
+  const onSubmit = (data) => {
+    axios.post("https://stormy-basin-87659.herokuapp.com/services", data).then((res) => {
+      if (res.data.insertedId) {
+        alert("Successfully added your new room service.. Thank you for add  extra room. Please! check your all service");
+        reset();
+      }
+    });
+  };
   return (
-    <div>
-      <h1>AddServices</h1>
+    <div className="addServices">
+      <h3 className="text-custom-color text-center mb-5">Add Your New Room Services</h3>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input placeholder="What's room services name" {...register("name")} />
+        <input placeholder="Twin room or double room?" {...register("subName")} />
+        <input placeholder="Shift" {...register("shift")} />
+        <input placeholder="Image url" {...register("img")} />
+        <input placeholder="Price" type="number" {...register("price")} />
+        <textarea placeholder="Description" {...register("desc")} rows={4} />
+        <input className="add-submit" type="submit" value="Add Your Room" />
+      </form>
     </div>
   );
 };
--- src/components/Pages/ReviewOrder/ReviewOrder.js
+++ src/components/Pages/ReviewOrder/ReviewOrder.js
@@ -10,6 +10,24 @@ const ReviewOrder = () => {
       .then((res) => res.json())
       .then((data) => setOrders(data));
   }, []);
+
+  // delete orders
+
+  const handleDelete = (id) => {
+    const url = `https://stormy-basin-87659.herokuapp.com/reviewOrder/${id}`;
+    fetch(url, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.deletedCount) {
+          window.confirm("Are you sure you want to delete?");
+        }
+        const remaining = orders.filter((order) => order._id !== id);
+        setOrders(remaining);
+      });
+  };
+
   return (
     <div className="mt-5 container">
       <Table striped bordered hover>
@@ -40,7 +58,9 @@ const ReviewOrder = () => {
               </td>
               <td className="fw-bold">${order.price}</td>
               <td className="text-center">
-                <button className="btn border-0 btn-danger m-1">X</button>
+                <button onClick={() => handleDelete(order._id)} className="btn border-0 btn-danger m-1">
+                  X
+                </button>
               </td>
             </tr>
           ))}
