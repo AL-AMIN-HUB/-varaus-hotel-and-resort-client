1b0d6409ef2c02fbdd501517c59e1dd320920a92
Md-Alamin-hub <86272640+Md-Alamin-hub@users.noreply.github.com>
2021-10-31 20:34:38 +0600

    project done

--- src/components/Pages/Service/Service.js
+++ src/components/Pages/Service/Service.js
@@ -2,13 +2,9 @@ import React from "react";
 import { Button, Card, Col } from "react-bootstrap";
 import "./Service.css";
 import { Link } from "react-router-dom";
-import axios from "axios";
 
 const Service = (props) => {
   const { name, desc, img, price, shift, subName, _id } = props.service;
-  const OrderPlaced = (data) => {
-    axios.post("https://stormy-basin-87659.herokuapp.com/reviewOrder", data).then((res) => console.log(res.data));
-  };
   return (
     <div>
       <Col>
@@ -28,9 +24,7 @@ const Service = (props) => {
                   Price: <span className="fs-5 fw-bold">${price}</span> / Per <span>{shift}</span>
                 </Card.Text>
                 <Link to={`/orderPlace/${_id}`}>
-                  <Button onClick={() => OrderPlaced(props.service)} className="bg-custom-color border-0 py-2 px-3 fs-5">
-                    Booking Now
-                  </Button>
+                  <Button className="bg-custom-color border-0 py-2 px-3 fs-5">Booking Now</Button>
                 </Link>
               </Card.Body>
             </div>
